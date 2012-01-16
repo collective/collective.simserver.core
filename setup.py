39e73674f58a30bd99fbb69059dfc78dae83aa4a
Christian Ledermann <christian.ledermann@gmail.com>
2012-01-16 19:30:49 +0300

    initial revision

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='collective.simserver.core',
+      version=version,
+      description="Plone integration for simserver",
+      long_description=open("README").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Christian Ledermann',
+      author_email='christian.ledermann@gmail.com',
+      url='https://github.com/cleder/collective.simserver.core',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.simserver'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'Pyro4',
+          'plone.app.registry',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
